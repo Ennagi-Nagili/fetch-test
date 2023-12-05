fc36a0e0638d877a966f22d03908727b6fe9e516
Ennagi-Nagili <nagili2004@gmail.com>
2023-12-05 16:29:43 +0400

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [page , stePage] = useState<number>(1)
+  const [images , steImages] = useState<object[]>([])
+
+  function changePage(pageNum: number) {
+    stePage(pageNum)
+  }
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/albums/" + page + "/photos")
+      .then((response) => response.json())
+      .then((res) => steImages(res));
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="grid">
+        {images?.map((item: any) => (
+          <div className="container">
+            <p className="title">{item.title}</p>
+            <img src={item.url} alt="" className="image"/>
+          </div>
+        ))}
+      </div>
+
+      <div>
+        <div className="bottom">
+          <button onClick={() => {changePage(1)}}>1</button>
+          <button onClick={() => {changePage(2)}}>2</button>
+          <button onClick={() => {changePage(3)}}>3</button>
+          <button onClick={() => {changePage(4)}}>4</button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
