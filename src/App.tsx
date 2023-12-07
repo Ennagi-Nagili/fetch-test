9617d786bdbf30ff6c124cdd4debb62f27ba1e5d
Ennagi-Nagili <nagili2004@gmail.com>
2023-12-07 16:11:07 +0400

    infinite scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,79 @@
-import React, { useEffect, useState } from "react";
-import logo from "./logo.svg";
+import React, { lazy, useEffect, useState } from "react";
 import "./App.css";
+import InfiniteScroll from "react-infinite-scroller";
 
+type MyImage = {
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+}
 const App = () => {
-  const [page , stePage] = useState<number>(1)
-  const [images , steImages] = useState<object[]>([])
+  const [page, stePage] = useState<number>(1);
+  const [images, steImages] = useState<object[]>([]);
 
   function changePage(pageNum: number) {
-    stePage(pageNum)
+    stePage(pageNum);
   }
 
-  useEffect(() => {
+  function load() {
     fetch("https://jsonplaceholder.typicode.com/albums/" + page + "/photos")
       .then((response) => response.json())
-      .then((res) => steImages(res));
-  });
+      .then((res) => {
+        const img = images;
+        img.push(res);
+        steImages(img);
+        console.log(images[0]);
+      });
+  }
 
   return (
     <div>
-      <div className="grid">
-        {images?.map((item: any) => (
-          <div className="container">
-            <p className="title">{item.title}</p>
-            <img src={item.url} alt="" className="image"/>
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={load}
+        hasMore={true}
+        useWindow={true}
+        loader={
+          <div className="loader" key={0}>
+            Loading ...
           </div>
-        ))}
-      </div>
+        }
+      >
+        <img src={images[0].url} alt="" />
+      </InfiniteScroll>
 
       <div>
         <div className="bottom">
-          <button onClick={() => {changePage(1)}}>1</button>
-          <button onClick={() => {changePage(2)}}>2</button>
-          <button onClick={() => {changePage(3)}}>3</button>
-          <button onClick={() => {changePage(4)}}>4</button>
+          <button
+            onClick={() => {
+              changePage(1);
+            }}
+          >
+            1
+          </button>
+          <button
+            onClick={() => {
+              changePage(2);
+            }}
+          >
+            2
+          </button>
+          <button
+            onClick={() => {
+              changePage(3);
+            }}
+          >
+            3
+          </button>
+          <button
+            onClick={() => {
+              changePage(4);
+            }}
+          >
+            4
+          </button>
         </div>
       </div>
     </div>
